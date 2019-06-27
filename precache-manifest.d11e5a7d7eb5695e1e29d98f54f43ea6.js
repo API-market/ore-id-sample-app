0a1fc3b9e126eb855ee5efda8865dc354b46bed6
Daniel Lin <daniel@aikon.com>
2019-06-27 15:12:46 -0700

    Updates

---
+++ precache-manifest.d11e5a7d7eb5695e1e29d98f54f43ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fcdce167ac80a4211a25c3b0de207a0",
+    "url": "/ore-id-sample-app/index.html"
+  },
+  {
+    "revision": "9adc8d2366756bec8d18",
+    "url": "/ore-id-sample-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ore-id-sample-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9adc8d2366756bec8d18",
+    "url": "/ore-id-sample-app/static/js/main.1822c5f1.chunk.js"
+  },
+  {
+    "revision": "1ab1c5a246b27e5a4359",
+    "url": "/ore-id-sample-app/static/js/runtime~main.027c8a0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ore-id-sample-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
